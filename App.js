63e8d7a99fa0706f54d0afbf6a2274f671f7c5fc
Vitor César <vitorcesardc@gmail.com>
2023-05-10 22:12:09 -0300

    funcional

--- App.js
+++ App.js
@@ -1,88 +1,156 @@
 
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-
+import { TouchableOpacity } from 'react-native-web';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 export default function App() {
-  const  [content] = useState('123456789');
+  
+  const [currentNumber, setCurrentNumber ] = useState("")
+  const [ lastNumber, setLastNumber ] = useState("")
+
+  function calculator(){
+    const splitNumbers = currentNumber.split(" ")
+    const operator = splitNumbers[1]
+    console.log(splitNumbers)
+    if(operator === "×"){    
+      setCurrentNumber((parseFloat(splitNumbers[0]) * parseFloat(splitNumbers[2])).toString())
+      return
+    }
+    if(operator === "÷"){
+      setCurrentNumber((parseFloat(splitNumbers[0]) / parseFloat(splitNumbers[2])).toString())
+      return
+    }
+    if(operator === "+"){
+
+      setCurrentNumber((parseFloat(splitNumbers[0]) + parseFloat(splitNumbers[2])).toString())
+      return
+    }
+    if(operator === "-"){
+      setCurrentNumber((parseFloat(splitNumbers[0]) - parseFloat(splitNumbers[2])).toString())
+      return
+    }
+  }
+
+
+  function handleInput(keyboardPressed){
+
+    if(keyboardPressed === "="){
+      setLastNumber(currentNumber + " = ")
+      calculator()
+      return
+    }
+    if(keyboardPressed === "×" | keyboardPressed === "÷" | keyboardPressed === "+" | keyboardPressed === "-"){    
+      setCurrentNumber(currentNumber + " " + keyboardPressed + " ")      
+      return
+    }
+    if(keyboardPressed === "back"){
+      console.log(currentNumber)
+      setCurrentNumber(currentNumber.substring(0,(currentNumber.length - 1)))
+      return
+    }
+    if(keyboardPressed === "."){
+      setCurrentNumber(currentNumber + keyboardPressed) 
+      return
+    }
+    if(keyboardPressed === "+/-"){
+
+      return
+    }
+    if(keyboardPressed === "AC"){
+      setLastNumber("")
+      setCurrentNumber("")
+      return
+    }
+
+    setCurrentNumber(currentNumber + keyboardPressed)
+
+  }
+
+
+
+
 
   return (
     <View style={styles.container}>
       <View style={styles.display}>
-        <Text style={styles.text}>
-          {content}
+      <Text style={styles.text}>
+          {lastNumber}
+        </Text>
+        <Text style={styles.text4}>
+          {currentNumber}
         </Text>
       </View>
       <View style={styles.buttonSection}>
         <View style={styles.buttonRow}>
           <View style={styles.button} >
-            <Text style={styles.text2}>AC</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("AC")}><Text style={styles.text2}>AC</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text2}>+/-</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("+/-")}><MaterialCommunityIcons name="plus-minus-variant" size={24} color="#49efcb" /></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text2}>%</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("%")}><Text style={styles.text2}>%</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text3}>/</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("÷")}><Text style={styles.text3}>÷</Text></TouchableOpacity>
           </View>
         </View>
         <View style={styles.buttonRow}>
           <View style={styles.button} >
-            <Text style={styles.text}>7</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("7")}><Text style={styles.text}>7</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text}>8</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("8")}><Text style={styles.text}>8</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text}>9</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("9")}><Text style={styles.text}>9</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text3}>X</Text>
-          </View>
-        </View>
-        <View style={styles.buttonRow}>
-          <View style={styles.button}>
-            <Text style={styles.text}>4</Text>
-          </View>
-          <View style={styles.button} >
-            <Text style={styles.text}>5</Text>
-          </View>
-          <View style={styles.button} >
-            <Text style={styles.text}>6</Text>
-          </View>
-          <View style={styles.button} >
-            <Text style={styles.text3}>-</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("×")}><Text style={styles.text3}>×</Text></TouchableOpacity>
           </View>
         </View>
         <View style={styles.buttonRow}>
           <View style={styles.button}>
-            <Text style={styles.text}>1</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("4")}><Text style={styles.text}>4</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text}>2</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("5")}><Text style={styles.text}>5</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text}>3</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("6")}><Text style={styles.text}>6</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text3}>+</Text>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("-")}><Text style={styles.text3}>-</Text></TouchableOpacity>
           </View>
         </View>
         <View style={styles.buttonRow}>
           <View style={styles.button}>
-          <AntDesign name="reload1" size={24} color="white" />
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("1")}><Text style={styles.text}>1</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text}>0</Text>
+            <TouchableOpacity style={styles.button} onPress={()=> handleInput("2")}><Text style={styles.text}>2</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text}>.</Text>
+            <TouchableOpacity style={styles.button} onPress={()=> handleInput("3")}><Text style={styles.text}>3</Text></TouchableOpacity>
           </View>
           <View style={styles.button} >
-            <Text style={styles.text3}>=</Text>
+            <TouchableOpacity style={styles.button} onPress={()=> handleInput("+")}><Text style={styles.text3}>+</Text></TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.buttonRow}>
+          <View style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("back")}><AntDesign name="reload1" size={24} color="white" style={{ transform: [{ scaleX: -1 }]}}/></TouchableOpacity>
+          </View>
+          <View style={styles.button} >
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("0")}><Text style={styles.text}>0</Text></TouchableOpacity>
+          </View>
+          <View style={styles.button} >
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput(".")}><Text style={styles.text}>.</Text></TouchableOpacity>
+          </View>
+          <View style={styles.button} >
+          <TouchableOpacity style={styles.button} onPress={()=> handleInput("=")}><Text style={styles.text3}>=</Text></TouchableOpacity>
           </View>
         </View>
       </View>
@@ -137,6 +205,11 @@ const styles = StyleSheet.create({
   text3: {
     color: '#e16477',
     fontSize: 20,
+    fontWeight: 600
+  },
+  text4: {
+    color: 'white',
+    fontSize: 50,
     fontWeight: 600
   },
   display: {
